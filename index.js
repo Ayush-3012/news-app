eeedd51681edd079dcb5adfa663e87263b2777d4
Ayush-3012 <diveshsrivastava31@gmail.com>
2023-07-18 23:41:45 +0530

    created webpage using html, css, bootstrap

---
+++ index.js
@@ -0,0 +1,19 @@
+// const API_KEY = "33780e04ab8b4142bf17417cda780f46";
+
+// const url = "https://newsapi.org/v2/everything?q=";
+
+// // window.addEventListener("load", () => fetchNews("India"));
+
+// async function fetchNews(query) {
+//   const res = await fetch(`${url}${query}&apiKey=${API_KEY}`);
+//   const data = await res.json();
+//   bindData(data.articles);
+// }
+
+// function bindData(articles) {
+//   const newsCard = document.querySelector("#news-card");
+//     newsCard.innerHTML = "";
+//   articles.forEach((article) => {
+//     if (!article.urlToImage) return;
+//   });
+// }
