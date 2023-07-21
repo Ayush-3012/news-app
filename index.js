6957a327a0a35c45349300d028832268ce309b8d
Ayush-3012 <diveshsrivastava31@gmail.com>
2023-07-21 18:10:10 +0530

    cleared html, added logic to fetch the news from api and displayed over page

--- index.js
+++ index.js
@@ -1,19 +1,47 @@
 // const API_KEY = "33780e04ab8b4142bf17417cda780f46";
+const url = `https://newsapi.org/v2/everything?q=`;
 
-// const url = "https://newsapi.org/v2/everything?q=";
+const newsImages = new Array(4);
+const newsTitle = new Array(4);
+const newsDescription = new Array(4);
+const newsSource = new Array(4);
+for (var i = 0; i < 4; i++) {
+  newsImages[i] = document.querySelector(`#newsImage-${i + 1}`);
+  newsTitle[i] = document.querySelector(`#newsTitle-${i + 1}`);
+  newsDescription[i] = document.querySelector(`#newsDescription-${i + 1}`);
+  newsSource[i] = document.querySelector(`#newsSource-${i + 1}`);
+}
 
-// // window.addEventListener("load", () => fetchNews("India"));
+const options = {
+  method: "GET",
+  headers: {
+    "X-Api-Key": "33780e04ab8b4142bf17417cda780f46",
+  },
+};
 
-// async function fetchNews(query) {
-//   const res = await fetch(`${url}${query}&apiKey=${API_KEY}`);
-//   const data = await res.json();
-//   bindData(data.articles);
-// }
+const getNews = (category) => {
+  News.innerHTML = category;
+  fetch(url + category, options)
+    .then((response) => response.json())
+    .then((response) => {
+      console.log(response);
 
-// function bindData(articles) {
-//   const newsCard = document.querySelector("#news-card");
-//     newsCard.innerHTML = "";
-//   articles.forEach((article) => {
-//     if (!article.urlToImage) return;
-//   });
-// }
+      for (var i = 0; i < 4; i++) {
+        newsImages[i].src =
+          response.articles[i]["urlToImage"] != null
+            ? response.articles[i]["urlToImage"]
+            : "./newspaper.png";
+        newsTitle[i].innerHTML = response.articles[i]["title"];
+        newsDescription[i].innerHTML = response.articles[i]["content"];
+        newsSource[i].href = response.articles[i]["url"];
+      }
+    })
+    .catch((err) => console.error(err));
+};
+
+submit.addEventListener("click", (e) => {
+  e.preventDefault();
+  getNews(category.value);
+});
+
+window.addEventListener("load", () => getNews("World"));
